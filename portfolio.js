1c8ac199df3064d487c548352431e1f095c6b2f5
blessedev <blessedev185@gmail.com>
2026-03-21 22:17:59 -0700

    website update

--- portfolio.js
+++ portfolio.js
@@ -13,3 +13,29 @@ backToTop.addEventListener('click', ()=>{
         behavior: "smooth",
     });
 });
+
+
+//-\\  sideNavigationSlider
+const menuBtn = document.querySelector(".list")
+const sidebar = document.querySelector(".side-nav")
+const exit = document.querySelector('.exit')
+const sidenavLinks = document.querySelectorAll('.side-links')
+
+
+menuBtn.addEventListener('click', ()=>{
+    sidebar.classList.remove('close')
+    menuBtn.classList.add('lst-btn')
+    exit.classList.remove('ext')
+
+})
+exit.addEventListener('click', ()=>{
+    sidebar.classList.add('close')
+    menuBtn.classList.remove('lst-btn')
+    exit.classList.add('ext')
+    sidebar.display = 'none'
+})
+sidenavLinks.forEach(e =>(e.addEventListener('click', ()=> {
+    sidebar.classList.add('close')
+    menuBtn.classList.remove('lst-btn')
+    exit.classList.add('ext')
+})))
